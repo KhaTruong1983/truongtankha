477b570f01f1fd22364d6729cfabf9d140a497ba
KhaTruong1983 <tankha3000@gmail.com>
2025-11-30 23:04:46 +0700

    Add files via upload

---
+++ chuoi.py
@@ -0,0 +1,31 @@
+a = 'nhung ky tu trong nay la string trong a'
+b = "nhung ky tu trong nay la string trong b"
+
+c = '''
+nhung ky tu trong nay
+la chuoi nhieu dong 
+trong c'''
+
+d = """
+nhung ky tu trong nay
+la chuoi nhieu dong 
+trong d"""
+
+#escape sequence bao gom nhung lenh sau.
+
+# \a phat ra tieng bip
+# \b xoa 1 ky tu sau cung 
+# \n xuong dong
+# \t bang nhan nut tab 
+# \' in dau '
+# \" in dau "
+# \\ in dau \
+
+
+print (d)
+print(type(d))
+print ("vi du:" + ' i\'m a beginer')
+
+
+#chuoi tran la 1 chuoi co chu r phia truoc. chuoi tran se bo qua cac ky tu escap sequence trong chuoi VD: \n se khong duoc xuong hang
+print (r"\neu 1 ngay nao do")
